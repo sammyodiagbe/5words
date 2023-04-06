99ca8005da828ff0c6acee560dbb64faf9be3a54
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-04-06 10:37:06 -0500

    testing auth end points

--- routes/authentication-routes.js
+++ routes/authentication-routes.js
@@ -2,11 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/create-account", (req, res) => {
+router.post("/create-account", (req, res) => {
   res.send("Yo what is good");
 });
 
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
   res.send("Login request has been responded to.");
 });
 
